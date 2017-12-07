1d07b065d61fe343866a92f830fcde6b3977385a
Quentin MOREAU <quentin.moreau@etu.unice.fr>
2017-12-07 22:22:26 +0100

    Pour youyou

--- core/src/com/polytech/nuitinfo/Intersection.java
+++ core/src/com/polytech/nuitinfo/Intersection.java
@@ -1,5 +1,7 @@
 package com.polytech.nuitinfo;
 
+import java.util.ArrayList;
+
 /**
  * Created by Barnabé on 12/7/2017.
  */
@@ -7,19 +9,24 @@ package com.polytech.nuitinfo;
 class Intersection {
     private Trait trait1;
     private Trait trait2;
+    private int position;
 
-    public Intersection(Trait trait1, Trait trait2){
+    public Intersection(Trait trait1, Trait trait2, int position){
         this.trait1 = trait1;
         this.trait2 = trait2;
     }
 
-    private boolean checkIntersection(){
+    private boolean checkIntersection(ArrayList<Intersection> listeIntersection){
         for(Intersection i: listeIntersection){
-            if(i.isCrossed()){
-                character.setTrait(i.traitOppose(character.getTrait));
+            if(i.isCrossed(position)){
+                character.setTrait(i.traitOppose(character.getTrait()));
                 return true;
             }
         }
         return false;
+    }
+
+    private boolean isCrossed(int position){
+
     }
 }
